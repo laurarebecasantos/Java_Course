e90949bf3c3bec13a4c7b1e64889a0f6e0012070
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-01 18:55:55 -0300

    Initialing of variables String, int, double, and boolean whit value

---
+++ src/io/github/laurarebecasantos/lesson0005/Lesson0005.java
@@ -0,0 +1,27 @@
+package io.github.laurarebecasantos.lesson0005;
+
+class Lesson0005 {
+	
+	public static void main(String[] args) {
+		
+		int inteiro = 0;
+		double fracao = 0;
+		String t = "texto";
+		boolean boo1 = true;
+		
+		System.out.println(t);
+		
+		/*Obs.: Lesson05
+		 * Sobre variavel: int idade; -> inicializando variavel sem valor..
+		 * int idade = 27; -> inicializando variavel com valor.. 
+		 * int idade = null; -> inicializando com valor default..
+		 * 
+		 * !Importante: Validar convenção de nomeclaturas Java.
+		 * 
+		 * Ao iniciar a variavel com valor, é possivel imprimir ela dentro do println 
+		 * chamando-a pelo nome, pois ela foi inicializada.
+		 * 
+		 */
+	}
+}
+
