6a31900e8ba14456aeaf42866acb32a980373042
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-01 01:37:44 -0300

    Created to variable int and String.

--- src/io/github/laurarebecasantos/lesson0004/Lesson0004.java
+++ src/io/github/laurarebecasantos/lesson0004/Lesson0004.java
@@ -2,4 +2,16 @@ package io.github.laurarebecasantos.lesson0004;
 
 public class Lesson0004 {
 
+	public static void main(String[] args) {
+		
+	
+		int num = 100;
+		String s = "aaa";
+		
+		/* Obs.: Lesson04
+		 * Sobre variavel: tipo, nome, tamanho, valor. 
+		 * Sobre memoria: volatil, não fixa valores.
+		 * 
+		 */
+	}
 }
