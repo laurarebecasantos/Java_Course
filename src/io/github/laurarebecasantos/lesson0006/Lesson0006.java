6bc1e7542b2a2ec0390215825ff398e78455e21c
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-01 19:30:30 -0300

    Printing mathematical operations

---
+++ src/io/github/laurarebecasantos/lesson0006/Lesson0006.java
@@ -0,0 +1,25 @@
+package io.github.laurarebecasantos.lesson0006;
+
+class Lesson0006 {
+
+	public static void main (String[] args) {
+		
+		int num1, num2;
+		
+		num1 = 100;
+		num2 = num1 + 100;
+		
+		System.out.println("The result is: " + num2);
+		System.out.println("The result for variable num1 is: " + num1);
+		
+		String s = "The result for variable num1 + num2 /2 is: ";
+		int result = (num1 + num2) / 2;
+		
+		System.out.println( s + result);
+		
+		/*
+		 * Initializing two variables for operation math.
+		 * 
+		 */
+	}
+}
