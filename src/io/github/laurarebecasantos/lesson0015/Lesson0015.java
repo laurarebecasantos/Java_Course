fc7917e67aa9dbad9fe47b6c4e495bb5a24a50e9
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-03 14:03:34 -0300

    Caracteristics constant

---
+++ src/io/github/laurarebecasantos/lesson0015/Lesson0015.java
@@ -0,0 +1,23 @@
+package io.github.laurarebecasantos.lesson0015;
+
+class Lesson0015 {
+
+	public static void main(String[] args) {
+
+		int valueVariable = 10; // Variable
+		final int valueConstant = 195; // Constant
+
+		System.out.println("The variable that changes value: " + valueVariable);
+		System.out.println("The constant doesn't change: " + valueConstant);
+		
+		/*
+		 * Lesson0015 
+		 * Caracteristics constant 
+		 * 1. Constant: The value does not change 
+		 * 2. Constant: Has the memory address protected 
+		 * 3. Constant: Never deallocate
+		 * 4. Constant: Is declared once, but is always used
+		 * 
+		 */
+	}
+}
