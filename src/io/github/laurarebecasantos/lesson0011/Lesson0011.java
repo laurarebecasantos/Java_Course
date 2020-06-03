7d6090e63372df65ae89e656b0245ceaffe3fc8b
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-02 22:37:04 -0300

    Using variable boolean for decision

---
+++ src/io/github/laurarebecasantos/lesson0011/Lesson0011.java
@@ -0,0 +1,24 @@
+package io.github.laurarebecasantos.lesson0011;
+
+class Lesson0011 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * boolean b; b = 1==1; b = 1!=1; b = 1>2; b = 1<2;
+		 */
+
+		int n1 = 3;
+		int n2 = 3;
+
+		System.out.println(n1 >= n2);
+
+		/*
+		 * Lesson0011 
+		 * Using variable boolean for decision. 
+		 * Important: Check condition operator types
+		 */
+
+	}
+
+}
