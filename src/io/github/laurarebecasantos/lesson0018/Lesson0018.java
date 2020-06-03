bcb99463ed9ef0e4e1123ff2f73cb1a124df7804
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-03 18:38:27 -0300

    Types primitive I

---
+++ src/io/github/laurarebecasantos/lesson0018/Lesson0018.java
@@ -0,0 +1,40 @@
+package io.github.laurarebecasantos.lesson0018;
+
+class Lesson0018 {
+	
+	public static void main(String[] args) {
+		
+		
+		/*
+		 * By variable pattern of the boolean 
+		 * type it will always be false 
+		 */
+		boolean b = true; 
+		
+		/* By default or char is equal ''
+		 * occupies 2 bytes, 16 bits
+		 */
+		char c = 'a';
+		char c2 = '1';
+		
+		
+		
+		
+		/* Lesson0018
+		 * Types primitive:
+		 * char
+		 * boolean
+		 * byte
+		 * short
+		 * int
+		 * long
+		 * float
+		 * double
+		 * 
+		 * It is necessary to validate how much memory or program 
+		 * I need to use to adapt variables to the appropriate type.
+		 * 
+		 */
+	}
+
+}
