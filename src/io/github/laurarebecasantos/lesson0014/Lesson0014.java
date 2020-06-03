e1cc9e8bed3b9139daab95b3e188aaf342b9bc67
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-03 13:11:09 -0300

    Testing rest of the division

---
+++ src/io/github/laurarebecasantos/lesson0014/Lesson0014.java
@@ -0,0 +1,17 @@
+package io.github.laurarebecasantos.lesson0014;
+
+class Lesson0014 {
+
+	public static void main(String[] args) {
+				
+		System.out.println(2 % 2);
+		System.out.println(7 % 2);
+		System.out.println(100 % 3);
+		System.out.println(1000 % 7);
+		System.out.println(3450 % 6.3);
+
+		/* Lesson0014
+		 * Testing rest of the division
+		 */
+	}
+}
