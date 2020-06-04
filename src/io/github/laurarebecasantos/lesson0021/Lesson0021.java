0d3267936406c5d208b07fd4324fd991386340df
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-03 22:48:04 -0300

    While structure

---
+++ src/io/github/laurarebecasantos/lesson0021/Lesson0021.java
@@ -0,0 +1,35 @@
+package io.github.laurarebecasantos.lesson0021;
+
+class Lesson0021 {
+
+	public static void main(String[] args) {
+
+		
+		int i = 10;
+		/*
+		while(i <= 0){
+			i = i + 1;
+			System.out.println(i);
+		}
+		*/
+		while(i >= 0) {
+			System.out.println(i);
+			i = i - 1;
+		}
+		
+		
+		/*
+		 * inteira x = 0;
+		 * 
+		 * enquanto(x<=9){ 
+		 * inteira = inteira +1; incremento na nossa variavel
+		 * imprima("essa msg"); }
+		 * 
+		 */
+
+		/* Lesson0021 
+		 * While Structure 
+		 * in portugol
+		 */
+	}
+}
