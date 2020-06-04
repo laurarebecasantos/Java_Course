a6fea88310ee1984714e58c0e228da53a855fa32
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-04 15:44:59 -0300

    Looping For II

---
+++ src/io/github/laurarebecasantos/lesson0030/Lesson0030.java
@@ -0,0 +1,35 @@
+package io.github.laurarebecasantos.lesson0030;
+
+public class Lesson0030 {
+
+	public static void main(String[] args) {
+		
+		/*
+		for (int i = 0; i <= 3; ++i ) {
+			System.out.println(i);
+		}
+		*/
+		
+		
+		for(int a=0, b=10; a<=10; ++a, --b) {
+			System.out.print(a);
+			System.out.print("-");
+			System.out.print(b);
+			System.out.println();
+			
+			
+		}
+		
+		
+		/* Lesson0030
+		 * 
+		 * Looping FOR
+		 * 
+		 * Part 1: where we declare the variable / initialing
+		 * Part 2: where we condition the looping / expression
+		 * Part 3: where we increase the variable / actualing for
+		 * 
+		 * 
+		 */
+	}
+}
