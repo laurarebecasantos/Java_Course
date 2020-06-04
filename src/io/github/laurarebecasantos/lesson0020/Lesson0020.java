94fa17de4b23a2ffa7188c0cfb874c38e65821f0
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-03 21:35:24 -0300

    Creating a loop

---
+++ src/io/github/laurarebecasantos/lesson0020/Lesson0020.java
@@ -0,0 +1,21 @@
+package io.github.laurarebecasantos.lesson0020;
+
+class Lesson0020 {
+
+	public static void main(String[] args) {
+		
+		
+			int x = 0;
+			
+			while(x<=9) {
+				x = x + 1;
+				System.out.println("Hi, i am a loop!");			
+			}
+		
+		
+		/* Lesson0020
+		 * Introduction to conditional loops
+		 * 
+		 */
+	}
+}
