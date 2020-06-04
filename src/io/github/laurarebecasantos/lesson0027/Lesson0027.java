b25215a664bc09846c6eac073ae119530f45b2e9
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-04 12:31:38 -0300

    Repeat loop Do/While

---
+++ src/io/github/laurarebecasantos/lesson0027/Lesson0027.java
@@ -0,0 +1,25 @@
+package io.github.laurarebecasantos.lesson0027;
+
+class Lesson0027 {
+
+	public static void main(String[] args) {
+		
+		int i = 0;
+		
+		do {
+			System.out.println("do/while");
+		}while(i != 0);
+		
+		while(i != 0) {
+			System.out.println("while");
+		}
+		
+		/* Lesson0027
+		 * repeat loop Do/While
+		 * 
+		 * Unlike While that only executes if the 
+		 * expression is true, Do While executes at least once.
+		 * 
+		 */
+	}
+}
