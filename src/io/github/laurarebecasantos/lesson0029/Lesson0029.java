4a7dc3d2ba412eb0fa3def3e86b0433ed740522b
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-04 14:06:07 -0300

    Looping For

---
+++ src/io/github/laurarebecasantos/lesson0029/Lesson0029.java
@@ -0,0 +1,26 @@
+package io.github.laurarebecasantos.lesson0029;
+
+import java.util.Scanner;
+
+class Lesson0029 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i <= 10; ++i) {
+			
+			System.out.println( i );
+		}
+		
+		
+		
+		
+		/* Lesson0029
+		 * Looping FOR
+		 * 
+		 * Part 1: where we declare the variable
+		 * Part 2: where we condition the looping
+		 * Part 3: where we increase the variable
+		 * 
+		 */
+	}
+}
