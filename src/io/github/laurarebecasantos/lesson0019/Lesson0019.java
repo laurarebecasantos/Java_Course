ddc8e6e58ffaef3d85e901ebbfac576f477f152f
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-03 21:25:09 -0300

    Types primitive II

---
+++ src/io/github/laurarebecasantos/lesson0019/Lesson0019.java
@@ -0,0 +1,51 @@
+package io.github.laurarebecasantos.lesson0019;
+
+public class Lesson0019 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * By variable pattern of the boolean 
+		 * type it will always be false 
+		 */
+		boolean b = true; 
+		
+		/* By default or char is equal ''
+		 * occupies 2 bytes, 16 bits
+		 */
+		char c = 'a';
+		char c2 = '1';
+		
+		/* By default java assigns 0 to our variable
+		 * Byte occupies 1 byte in memory
+		 * -128 e +127
+		 */
+		byte Byte = 100;
+		byte Byte2 = -128;
+		
+		/* By default java assigns 0 to our variable
+		 * -32768 +32767
+		 */
+		short shirt = 2000;
+		
+		 /* By default java assigns 0 to our variable
+		  * -2,147,483,648 to +2,147,486,647
+		  * 
+		  */
+		int i = 1;
+		
+		long l;
+		
+		float f = 4.4f;
+		double d = 2.2;
+		
+		
+		
+		
+		/* Lesson0019
+		 * 
+		 */
+		
+	}
+	
+}
