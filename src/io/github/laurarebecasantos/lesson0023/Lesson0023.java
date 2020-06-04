1197c329378a423356ab61166ae87098fa9aef5c
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-03 23:27:08 -0300

    Operators associativity

---
+++ src/io/github/laurarebecasantos/lesson0023/Lesson0023.java
@@ -0,0 +1,30 @@
+package io.github.laurarebecasantos.lesson0023;
+
+class Lesson0023 {
+
+	public static void main(String[] args) {
+		
+		int x, y, z;
+		x = y = z = 0;
+		
+		int a = (x + y) + z;
+		
+		
+		/* Lesson0023
+		 * Operators associativity
+		 * 
+		 * Operators table Java
+		 * From right to left
+		 * 
+		 * To assign a value to the variable, 
+		 * java reads from right to left;
+		 * 
+		 * 1) ()
+		 * 2) *, /, %
+		 * 3) +, -
+		 * 4) <, <=, >, >=
+		 * 5) ==, !=
+		 */
+	}
+	
+}
