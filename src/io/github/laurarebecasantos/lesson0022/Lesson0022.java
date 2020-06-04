97ac8d60d7339da8910768f818da58610867ffb1
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-03 22:59:12 -0300

    A looping program error

---
+++ src/io/github/laurarebecasantos/lesson0022/Lesson0022.java
@@ -0,0 +1,20 @@
+package io.github.laurarebecasantos.lesson0022;
+
+class Lesson0022 {
+
+	public static void main(String[] args) {
+		
+		int x = 0;
+		while(x<=10) {
+			x = x -1;
+			System.out.println(x);
+		}
+		
+		
+		
+		/* Lesson0022
+		 * A program looping
+		 * Looping condition per increment error
+		 */
+	}
+}
