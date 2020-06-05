b8d66ea7a6c314686bd73ba34f66a7438f9612e8
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-05 08:25:23 -0300

    Instruction block

---
+++ src/io/github/laurarebecasantos/lesson0031/Lesson0031.java
@@ -0,0 +1,27 @@
+package io.github.laurarebecasantos.lesson0031;
+
+class Lesson0031 {
+
+	public static void main(String[] args) {
+		int e = 0;
+		{
+			{
+				int i;
+			}
+			
+			e =1;  
+		}
+		
+	//	i = 0;
+		
+		
+		
+		/* Lesson0031
+		 * Instruction block
+		 * 
+		 * The instruction block is used 
+		 * to join the parts of a program.
+		 */
+	}
+	
+}
