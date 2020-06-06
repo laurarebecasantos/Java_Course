1d62b6671be8d8d2e2d260486add62ce4246d61c
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-06 11:34:47 -0300

    Instruction block II

--- src/io/github/laurarebecasantos/lesson0032/Lesson0032.java
+++ src/io/github/laurarebecasantos/lesson0032/Lesson0032.java
@@ -5,7 +5,21 @@ class Lesson0032 {
 	public static void main(String[] args) {
 		
 		
-		/*
+		int i = 0;
+		while(i <=10);
+		{
+			++i;
+		}
+		
+		while(i <=10) {
+			++i;
+		}
+		
+		
+		/* Lesson0032
+		 * Instruction block II
+		 * 
+		 * 
 		 * 
 		 * 
 		 * 
