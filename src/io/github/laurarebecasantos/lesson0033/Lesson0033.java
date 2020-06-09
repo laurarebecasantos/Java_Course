d9e88ebd8408a9161c8d77aa19481c1fa6496a4f
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-08 22:38:35 -0300

    Random class

---
+++ src/io/github/laurarebecasantos/lesson0033/Lesson0033.java
@@ -0,0 +1,29 @@
+package io.github.laurarebecasantos.lesson0033;
+
+import java.util.Random;
+
+class Lesson0033 {
+
+	public static void main(String[] args) {
+		
+		Random num = new Random();
+		for(int i = 0; i<=6; ++i)
+			/*
+			System.out.println(num.nextInt(6)+1); //0...5
+			System.out.println(num.nextDouble()*50); //...50
+			System.out.println(num.nextDouble()*50+20); //20...50
+			*/
+			System.out.println(num.nextBoolean());
+			
+	
+		
+		
+		/* Lesson033
+		 * Random class
+		 * 
+		 * Types of variables with random class
+		 * 
+		 * 
+		 */
+	}
+}
