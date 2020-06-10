0f079e3a283458e47ab454fcbcf723dd6cb27045
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-09 22:57:42 -0300

    Ternary condition

---
+++ src/io/github/laurarebecasantos/lesson0034/Lesson0034.java
@@ -0,0 +1,40 @@
+package io.github.laurarebecasantos.lesson0034;
+
+import java.util.Scanner;
+
+class Lesson0034 {
+
+	public static void main(String[] args) {
+
+		int a, b;
+		String value;
+
+		a = 5;
+		b = 6;
+
+		/*
+		 * if (a == b)
+		 * 
+		 * value = "true";
+		 * 
+		 * else
+		 * 
+		 * value = "false";
+		 */
+		
+		value = (a>b) ? "true" : "false";
+		
+		// String variable = (condition) ? "value true" : "value false";
+
+		System.out.println(value);
+		
+		/* Lesson0034
+		 * 
+		 * Ternary condition
+		 * 
+		 * 
+		 * 
+		 */
+
+	}
+}
