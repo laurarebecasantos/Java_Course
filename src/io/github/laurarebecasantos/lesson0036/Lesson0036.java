fe28127f3d23adbaf3112e717ed877c01f9eb5d1
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-11 15:44:36 -0300

    Break instruction

---
+++ src/io/github/laurarebecasantos/lesson0036/Lesson0036.java
@@ -0,0 +1,51 @@
+package io.github.laurarebecasantos.lesson0036;
+
+class Lesson0036 {
+	public static void main(String[] args) {
+		
+		//looping for:
+		for (int i = 0; i<10; i++) {
+			if (true)
+				break;
+		}
+		
+		//looping while:
+		int i = 10;
+		while(i<100) {
+			i++;
+			if(true)
+				break;
+		}
+		
+		//Using the switch statement
+		int dia = 2;
+		switch (dia) {
+		case 1:
+			System.out.println("Sunday");
+			break;
+		case 2:
+			System.out.println("Monday");
+			break;
+		default:
+			System.out.println("Invalid day");
+		}
+		
+		//instruction block label with
+		block:{
+			System.out.println("1");
+			if(true) 
+				break block;
+			System.out.println("5");
+		}
+		
+		
+		/* Lesson0036
+		 * Break instruction
+		 * Continue instruction
+		 * 
+		 * To end a repeat loop
+		 * 
+		 */
+	}
+
+}
