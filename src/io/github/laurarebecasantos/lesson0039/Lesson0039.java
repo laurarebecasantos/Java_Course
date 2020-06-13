10e74171a2b5238102f4ffc943f294c5924e17ab
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-12 23:52:58 -0300

    Continue instruction

---
+++ src/io/github/laurarebecasantos/lesson0039/Lesson0039.java
@@ -0,0 +1,22 @@
+package io.github.laurarebecasantos.lesson0039;
+
+class Lesson0039 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i<10; i++) {
+			if(true)
+				continue;
+			
+		}
+		
+		
+	}
+	
+	/* Lesson0039
+	 * Continue instruction
+	 * 
+	 * 
+	 * 
+	 */
+}
