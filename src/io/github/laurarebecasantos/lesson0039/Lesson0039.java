18bfa3fa5fb7c6ccd4eb4467134ce051fc1a16e9
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-13 23:17:22 -0300

    Continue instruction

--- src/io/github/laurarebecasantos/lesson0039/Lesson0039.java
+++ src/io/github/laurarebecasantos/lesson0039/Lesson0039.java
@@ -3,18 +3,30 @@ package io.github.laurarebecasantos.lesson0039;
 class Lesson0039 {
 
 	public static void main(String[] args) {
-		
-		for (int i = 0; i<10; i++) {
-			if(true)
+
+		/*
+		 * for (int i = 0; i<10; i++) { if(true) continue;
+		 * 
+		 * }
+		 */
+
+		System.out.println("I will print the even values from 0 to 10: ");
+		for (int i=0; i<100; i++) {
+			if (1%2 == 0)
+				System.out.println(i);
+			else
 				continue;
-			
+
+			System.out.println("-");
 		}
-		
-		
 	}
-	
-	/* Lesson0039
-	 * Continue instruction
+
+	/*
+	 * Lesson0039 Continue instruction
+	 * 
+	 * formula to know if the value of the variable is divided by 2, and is even or
+	 * not (1%2==0)
+	 * 
 	 * 
 	 * 
 	 * 
