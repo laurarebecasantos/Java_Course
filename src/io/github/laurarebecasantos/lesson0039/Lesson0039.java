bc71029f3a6e57cfe063da89190202f53bfea40c
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-16 23:26:01 -0300

    Continue instruction

--- src/io/github/laurarebecasantos/lesson0039/Lesson0039.java
+++ src/io/github/laurarebecasantos/lesson0039/Lesson0039.java
@@ -18,19 +18,18 @@ class Lesson0039 {
 		 */
 
 		System.out.println("I will print the even values from 0 to 10: ");
-		for (int i = 0; i <= 100; i++) {
+		for (int i = 0; i < 100; i++) {
 			if (i % 2 == 0)
 				System.out.println(i);
 			else
 				continue;
 		int a = 0, soma = 0;
-		while(a<=i) {
-			i++;
-			
-			soma += i;
+		while(a<i) {
+			a++;
+			soma += a;
 		}
 		
-		System.out.println();
+		System.out.println("The sum of all numbers is: " + soma);
 		}
 	}
 
@@ -40,8 +39,7 @@ class Lesson0039 {
 	 * formula to know if the value of the variable is divided by 2, and is even or
 	 * not (1%2==0)
 	 * 
-	 * 
-	 * 
+	 * executing the loop internally and the loop adds up all the numbers until a is less than i, but only if the number is even, even if the instruction continues, does it end execution.
 	 * 
 	 */
 }
