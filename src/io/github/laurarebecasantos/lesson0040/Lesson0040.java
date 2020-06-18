62b5a58ad639687b69e3548120864d2bf959c465
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-17 23:30:16 -0300

    Reserved words

--- src/io/github/laurarebecasantos/lesson0040/Lesson0040.java
+++ src/io/github/laurarebecasantos/lesson0040/Lesson0040.java
@@ -2,4 +2,29 @@ package io.github.laurarebecasantos.lesson0040;
 
 class Lesson0040 {
 
+	public static void main(String[] args) {
+		
+		
+		/* Lesson0040
+		 * reserved words
+		 * 
+		 * reserved words already used:
+		 * do, long, boolean, char, double, break, else,
+		 * float, switch, while, byte, continue, for, int,
+		 * null, true, case, false, if, package
+		 * 
+		 * reserved words not used: [for OO]
+		 * catch, abstract, final, implements, private, static, throw, void, finally,
+		 * import, protected, super, throws, class, instanceof, new, public, extends,
+		 * return, default, interface
+		 * 
+		 * not used:
+		 * native, volatile, transient, synchronized
+		 * 
+		 * 
+		 */
+		
+		
+		
+	}
 }
