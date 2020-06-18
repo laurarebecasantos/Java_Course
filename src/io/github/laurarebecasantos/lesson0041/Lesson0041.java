165028ece4c467ab3e320ca88ab5c1e476d6a5b6
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-17 23:51:09 -0300

    Array

---
+++ src/io/github/laurarebecasantos/lesson0041/Lesson0041.java
@@ -0,0 +1,23 @@
+package io.github.laurarebecasantos.lesson0041;
+
+class Lesson0041 {
+
+	public static void main(String[] args) {
+		
+	int[] arrInt = new int[5];
+		
+		//Position 0 to 4
+		//Element from 0 to 5
+		
+		/* Lesson0041
+		 * Array
+		 * 
+		 * Array is a data structure that allows the manipulation 
+		 * of a finite number of elements of the same type.
+		 * 
+		 * In java, array is an ordered collection of elements.
+		 * 
+		 */
+	}
+	
+}
