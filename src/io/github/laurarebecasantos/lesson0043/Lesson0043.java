e5964e07e748a4a77494dc624f66737b1530f7d7
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-18 23:48:13 -0300

    Array III

---
+++ src/io/github/laurarebecasantos/lesson0043/Lesson0043.java
@@ -0,0 +1,27 @@
+package io.github.laurarebecasantos.lesson0043;
+
+class Lesson0043 {
+
+	public static void main(String[] args) {
+		
+		// int[] arrInt = new int[5];
+		
+		int[] array;
+		array = new int[10];
+		
+		for (int i=0; i<=9; i++) {
+			System.out.printf("%5d%7d\n", i, array[i]);
+			
+			
+		}
+		
+		
+		/* Lesson0043
+		 * 
+		 * 
+		 * 
+		 */
+		
+	}
+	
+}
