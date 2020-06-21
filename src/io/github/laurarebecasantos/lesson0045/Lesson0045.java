f1d51151598fb99fbac8b1dadf5a5f0bd75d84fe
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-21 10:38:29 -0300

    One-dimensional arrays

---
+++ src/io/github/laurarebecasantos/lesson0045/Lesson0045.java
@@ -0,0 +1,27 @@
+package io.github.laurarebecasantos.lesson0045;
+
+class Lesson0045 {
+
+	public static void main(String[] args) {
+		
+		int[] array = {78, 20, 56, 89, 1};
+		System.out.printf("%s%8s\n", "Indice", "Valores");
+		
+		for(int i=0; i<array.length; i++)
+			System.out.printf("%5d%8d\n", i, array[i]);
+		
+		
+		
+		/* Lesson0045
+		 * One-dimensional arrays
+		 * 
+		 * 
+		 * How to print all the values
+		 * of an array.
+		 * 
+		 * 
+		 */
+	}
+	
+	
+}
