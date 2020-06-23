402797322b189648cae05afedc428df4c40d2c04
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-22 23:18:33 -0300

    Characteristics arrays

---
+++ src/io/github/laurarebecasantos/lesson0046/Lesson0046.java
@@ -0,0 +1,47 @@
+package io.github.laurarebecasantos.lesson0046;
+
+class Lesson0046 {
+
+	public static void main(String[] args) {
+		
+		
+		int [] a = new int[10];
+		a[0] = 000;
+		a[1] = 111;
+		a[2] = 222;
+		a[3] = 333;
+		a[4] = 444;
+		a[5] = 555;
+		a[6] = 666;
+		a[7] = 777;
+		a[8] = 888;
+		a[9] = 999;
+		
+		System.out.println(a[4]);
+		System.out.println(a[5]);
+		System.out.println(a[6]);
+		
+		/* Lesson0046
+		 * Characteristics
+		 * 
+		 * Every array is an object and this object 
+		 * has a list of elements and they make up the array.
+		 * 
+		 * Dimension - numbers of elements
+		 * Index - numbering starting at 0
+		 * Elements - items that make up the array
+		 * Type - array type
+		 * 
+		 * 
+		 * ps.:
+		 * int[] lista = new int[5];
+		 *
+		 *	lista[1] = 45;
+		 *	lista[2] = 10;
+		 *	lista[3] = 1;
+		 *	lista[4] = 1000;
+		 *	lista[5] = 45;
+		 */
+	}
+	
+}
