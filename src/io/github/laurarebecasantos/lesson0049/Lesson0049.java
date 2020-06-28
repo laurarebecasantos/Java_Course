673a599784fd69ca9245902b446c0a746e7f6b0f
laurarebecasantos <laurarebecasantos@gmail.com>
2020-06-27 23:37:39 -0300

    array lesson 01

--- src/io/github/laurarebecasantos/lesson0049/Lesson0049.java
+++ src/io/github/laurarebecasantos/lesson0049/Lesson0049.java
@@ -1,12 +1,24 @@
 package io.github.laurarebecasantos.lesson0049;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 class Lesson0049 {
 
 	public static void main(String[] args) {
 		
+		Scanner in = new Scanner(System.in);
+		int [] values = new int[7]; 
 		
 		
+				
+		for(int i=0; i<7; i++) {
+			
+			System.out.printf("Imput a number [%d]: ", i+1);
+			values[0] = in.nextInt();	
+		}
 		
+		System.out.println(Arrays.toString(values));
 	}
 	
 }
