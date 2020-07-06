32dc4d2955070bc941184704c618050fce0081bc
laurarebecasantos <laurarebecasantos@gmail.com>
2020-07-06 13:45:47 -0300

    Array revision unidimensional

--- src/io/github/laurarebecasantos/lesson0048/Lesson0048.java
+++ src/io/github/laurarebecasantos/lesson0048/Lesson0048.java
@@ -3,16 +3,43 @@ package io.github.laurarebecasantos.lesson0048;
 class Lesson0048 {
 
 	public static void main(String[] args) {
-		
+
 		String[] myArray = new String[5];
-		
-		/* Lesson0048
-		 * Array revision unidimensional
+
+		/*
+		 * Lesson0048 Array revision unidimensional
 		 * 
 		 * String array.
 		 * 
+		 * The variable meuArray is a reference (instance) to an array object. 
+		 * 
+		 * The reference contained in meuArray points to an instance of type array. 
+		 * 
+		 * When the NEW statement returned the instance for a new ARRAY, at that moment there is
+		 * a new object of type ARRAY in the memory, as well, There are 5 spaces
+		 * reserved for storing 5 independent integers.
+		 * 
+		 * 
 		 * Everything described in the notebook
 		 */
+		
+		
+	   /*
+		* Variavel primitiva armazena valores conforme a sua especificação.
+		*
+		* Variaval do tipo objeto armazenam o endereço(referencia) à uma instancia.
+		*
+		* int[] meuArray = new Int[85];
+		*
+		* String[] meyArrayString = new String[60];
+		*
+		* Object[] meuArrayObject = new Object[90];
+		*
+		*/
 	}
-	
+
 }
+
+
+
+
