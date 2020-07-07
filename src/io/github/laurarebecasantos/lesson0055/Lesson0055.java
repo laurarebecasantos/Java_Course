01382d5382504fd20706628e55a0ea50a078f01f
laurarebecasantos <laurarebecasantos@gmail.com>
2020-07-07 20:08:50 -0300

    For Each

--- src/io/github/laurarebecasantos/lesson0055/Lesson0055.java
+++ src/io/github/laurarebecasantos/lesson0055/Lesson0055.java
@@ -4,12 +4,21 @@ class Lesson0055 {
 
 	public static void main(String[] args) {
 		
+		int[] list = new int [2];
+		list[0] = 10;
+		list[1] = 20;
+		list[2] = 30;
+		
+		for(int item : list) {
+		}
+		
+		
 		
 		/* Lesson0055
 		 * For Each
 		 * 
+		 * It is a specialized repetition structure.
 		 * 
 		 */
 	}
-	
 }
