89e07184344a133e9a3cb5f9cbcd94233a7a978f
laurarebecasantos <laurarebecasantos@gmail.com>
2020-07-06 23:58:26 -0300

    For Each

---
+++ src/io/github/laurarebecasantos/lesson0055/Lesson0055.java
@@ -0,0 +1,15 @@
+package io.github.laurarebecasantos.lesson0055;
+
+class Lesson0055 {
+
+	public static void main(String[] args) {
+		
+		
+		/* Lesson0055
+		 * For Each
+		 * 
+		 * 
+		 */
+	}
+	
+}
