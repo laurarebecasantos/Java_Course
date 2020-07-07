f43d0e5ff33ef51a9c75d4abe4adf2f048ad3415
laurarebecasantos <laurarebecasantos@gmail.com>
2020-07-06 22:46:52 -0300

    Multidimensional arrays

---
+++ src/io/github/laurarebecasantos/lesson0052/Lesson0052.java
@@ -0,0 +1,23 @@
+package io.github.laurarebecasantos.lesson0052;
+
+class Lesson0052 {
+	
+	public static void main(String[] args) {
+		
+		int [][] table = new int [5][5];
+		table [0][0] = 25;
+		table [3][2] = 2;
+		table [5][4] = 10;
+		table [2][3] = 77;
+		table [4][1] = 0;
+		
+		/* Lesson0052
+		 * Multidimensional arrays
+		 * 
+		 * Multidimensional array declaration:
+		 * <type>[][]<name>=new<type>[column][line];
+		 * 
+		 */
+	}
+
+}
