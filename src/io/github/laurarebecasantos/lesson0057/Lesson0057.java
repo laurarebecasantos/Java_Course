0ecbb37d001af774e8a9a7ebfecc42619ce1890c
laurarebecasantos <laurarebecasantos@gmail.com>
2020-07-08 14:29:42 -0300

    Nested cycles

---
+++ src/io/github/laurarebecasantos/lesson0057/Lesson0057.java
@@ -0,0 +1,29 @@
+package io.github.laurarebecasantos.lesson0057;
+
+class Lesson0057 {
+	
+	public static void main(String[] args) {
+		
+		int[][] values = new int[5][5];
+		for(int x=0; x<values.length; x++) {
+			for(int y=0; y<values.length; y++) {
+				values[x][y]=9;
+				System.out.println(values[x][y]+" ");
+				System.out.println();
+			}
+			
+		}
+		
+		
+		/* Lesson0057
+		 * Nested cycles
+		 * 
+		 * Manipulating values in the multidimensional matrix
+		 * 
+		 */
+		
+	}
+	
+	
+
+}
