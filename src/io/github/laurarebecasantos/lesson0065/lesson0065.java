ddc607a19dbd5878385c88fa2ed9a2c701825a96
laurarebecasantos <laurarebecasantos@gmail.com>
2020-07-11 22:21:09 -0300

    Class declaration (Theory)

---
+++ src/io/github/laurarebecasantos/lesson0065/lesson0065.java
@@ -0,0 +1,27 @@
+package io.github.laurarebecasantos.lesson0065;
+
+class lesson0065 extends Object{
+	//Field
+	//Constructor
+	//Metods
+	
+	/* Class components:
+	:
+		1) Access modifier
+			a) public
+			b) private
+			c) protected
+			d) sem modificador
+
+		2) Assign a name (capital letter)
+		3) Extender of another class (extends)
+		4) Interface that ours will implement (implements)
+		5) The body of our class {}
+	*/
+	
+	/* Lesson0065
+	 * Class declaration
+	 * 
+	 * 
+	 */
+}
