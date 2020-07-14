7a6f5c9cc2169f9420d5a312927ec87c51ecc7e9
laurarebecasantos <laurarebecasantos@gmail.com>
2020-07-13 23:37:00 -0300

    Atributes of class

---
+++ src/io/github/laurarebecasantos/lesson0066/Lesson0066.java
@@ -0,0 +1,19 @@
+package io.github.laurarebecasantos.lesson0066;
+
+class Lesson0066 {
+
+		private boolean swirchedOd;
+		private boolean displayOn;
+		public int volume = 0;
+		
+		
+		
+		/* Lesson0066
+		 * Atributes of class
+		 * 
+		 * To be executed in the main class
+		 * 
+		 */
+	
+}
+  
