6362a93f53b0106e57214a51418bbd585a5dbe0c
laurarebecasantos <laurarebecasantos@gmail.com>
2020-07-14 22:33:45 -0300

    Atributes of class

--- src/io/github/laurarebecasantos/lesson0066/Lesson0066.java
+++ src/io/github/laurarebecasantos/lesson0066/Lesson0066.java
@@ -2,17 +2,21 @@ package io.github.laurarebecasantos.lesson0066;
 
 class Lesson0066 {
 
-		private boolean swirchedOd;
-		private boolean displayOn;
-		public int volume = 0;
-		
+		public static void main (String[] args) {
+			
+			Mobile mobile;
+			mobile = new Mobile();
+			mobile.volume = 5;
+	     // mobile.swirchedOd = true; private attribute, accessed only by the Mobile class
+			
+		}
 		
 		
 		/* Lesson0066
 		 * Atributes of class
 		 * 
 		 * To be executed in the main class
-		 * 
+		 * Mobile class instantiated in the main
 		 */
 	
 }
---
+++ src/io/github/laurarebecasantos/lesson0066/Mobile.java
@@ -0,0 +1,13 @@
+package io.github.laurarebecasantos.lesson0066;
+
+class Mobile {
+
+	private boolean swirchedOd;
+	private boolean displayOn;
+	public int volume = 0;
+	
+	/*
+	 * Attribute definition: [modifier] [type] name = [value];
+	 * 
+	 */
+}
