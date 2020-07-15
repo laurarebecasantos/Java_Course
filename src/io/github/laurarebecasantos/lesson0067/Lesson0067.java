011469583e7154fb805f5028ca800313c8c7f526
laurarebecasantos <laurarebecasantos@gmail.com>
2020-07-14 22:52:33 -0300

    Functions and Methods

---
+++ src/io/github/laurarebecasantos/lesson0067/Lesson0067.java
@@ -0,0 +1,21 @@
+package io.github.laurarebecasantos.lesson0067;
+
+class Lesson0067 {
+	
+	public static void method() {
+		System.out.println("Printed using a method.");
+	}
+	
+	public static void main(String[] args) {
+		
+		method();
+	}
+	
+	/* Lesson0067
+	 * Functions and Methods.
+	 * 
+	 * Basic structure for any method.
+	 * 
+	 */
+
+}
