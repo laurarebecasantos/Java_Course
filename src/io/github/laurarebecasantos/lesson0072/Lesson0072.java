96a183fde398c6f425bc38a6eccf547d2c8542a9
laurarebecasantos <laurarebecasantos@gmail.com>
2020-07-31 22:58:10 -0300

    Overloading

---
+++ src/io/github/laurarebecasantos/lesson0072/Lesson0072.java
@@ -0,0 +1,17 @@
+package io.github.laurarebecasantos.lesson0072;
+
+class overloading {
+	void print() {
+
+	}
+
+	void print(int a) {
+	}
+}
+
+class Lesson0072 {
+
+	public static void main(String[] args) {
+
+	}
+}
