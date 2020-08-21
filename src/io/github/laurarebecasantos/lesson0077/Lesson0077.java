212920727ddf5114ddc7e4015fe75b64608d3947
laurarebecasantos <laurarebecasantos@gmail.com>
2020-08-21 18:00:40 -0300

    Java exercises 01 - Printing text

---
+++ src/io/github/laurarebecasantos/lesson0077/Lesson0077.java
@@ -0,0 +1,27 @@
+package io.github.laurarebecasantos.lesson0077;
+
+class Lesson0077 {
+
+	public static void main(String[] args) {
+		/*
+		 * Make a program that prints the text "Hello world" for the user. So, print the
+		 * number 522 on the screen and then print a blank line. After printing the
+		 * blank line, write the following sentence on the screen:
+		 * 
+		 * "Welcome to the list of solved exercises"
+		 */
+
+		System.out.println("Hello world");
+		System.out.println(522);
+		System.out.println();
+		System.out.println("Welcome to the list of solved exercises");
+
+		/*
+		 * Lesson0077
+		 * Java exercises 01 - Printing text
+		 * 
+		 */
+
+	}
+
+}
