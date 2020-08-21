4a07b6521fe0412d3518dd8cca3e618e62f3dbe7
laurarebecasantos <laurarebecasantos@gmail.com>
2020-08-21 17:02:25 -0300

    Overloading

--- src/io/github/laurarebecasantos/lesson0072/Lesson0072.java
+++ src/io/github/laurarebecasantos/lesson0072/Lesson0072.java
@@ -1,17 +1,50 @@
 package io.github.laurarebecasantos.lesson0072;
 
-class overloading {
+class Overloading {
 	void print() {
 
 	}
 
 	void print(int a) {
 	}
-}
 
-class Lesson0072 {
+
+	void print(String s) {
+		System.out.println(s);
+	}
+	
+	void print(String s1, String s2) {
+		System.out.println(s1+" - "+s2);
+	}
+	void print(String s1, int i1) {
+		System.out.println(s1+" - "+i1);
+	}
+
+}
+public class Lesson0072 {
 
 	public static void main(String[] args) {
-
+/*
+		Overloading overlod1 = new Overloading();
+		Overloading overlod2 = new Overloading();
+		Overloading overlod3 = new Overloading();
+*/
+		//New form of class instance with method flame.
+		new Overloading().print("A String parameter");
+		new Overloading().print("First parameter","Second parameter");
+		new Overloading().print("Age is: " , 25);
+		
+		
+		/* Lesson0072
+		 * Overloading
+		 * 
+		 * Overload: combination of the function 
+		 *     name with the list of parameters it should receive
+		 * 
+		 * Unique set in each function
+		 * 
+		 * Differentiation by data type.
+		 * 
+		 */
 	}
 }
