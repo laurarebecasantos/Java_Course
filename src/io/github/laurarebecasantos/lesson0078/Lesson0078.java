77bb70467486b865416f78ed40edcc4c05e84d7d
laurarebecasantos <laurarebecasantos@gmail.com>
2020-08-22 22:02:48 -0300

    space/Java_Course (master) $ git commit -m

---
+++ src/io/github/laurarebecasantos/lesson0078/Lesson0078.java
@@ -0,0 +1,45 @@
+package io.github.laurarebecasantos.lesson0078;
+
+class Lesson0078 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Make a program that prints the sentence: 
+		 * "Welcome to my program!" 
+		 * Then print a blank line and in sequence, print the sentences and the result of
+		 * mathematical expressions.
+		 * 
+		 * Each sentence and its result must be on the same line.
+		 * "The 8 by 90 product is equal to:" 
+		 * "The 90 to 30 quotient is equal to:"
+		 * "The square of 9 is:"
+		 * 
+		 * Grades): 
+		 * 
+		 * 1) In each mathematical expression, you must print the result of an
+		 * expression in the which compiler solved it. That is, it is to concatenate the
+		 * phrase suggested in the statement with the result.
+		 * 
+		 */
+
+		System.out.println("Welcome to my program!");
+		System.out.println();
+		System.out.print("The 8 by 90 product is equal to: ");
+		System.out.println(8*90);
+		System.out.print("The 90 to 30 quotient is equal to: ");
+		System.out.println(90/30);
+		System.out.print("The square of 9 is: " + 9*9);
+		
+		
+		/*
+		 * Lesson0078 Java exercises 02 - Printing texts and numbers
+		 * 
+		 * System.out.print("\n") : skip the line
+		 * System.out.print : just prints
+		 * System.out.println : print and skip line
+		 */
+
+	}
+
+}
