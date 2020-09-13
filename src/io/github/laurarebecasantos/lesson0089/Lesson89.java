64a1ec370aa96d1db991c44257a77072ced0164c
laurarebecasantos <laurarebecasantos@gmail.com>
2020-09-13 18:58:04 -0300

    JAVA 8 - Lambdas Expressions

---
+++ src/io/github/laurarebecasantos/lesson0089/Lesson89.java
@@ -0,0 +1,22 @@
+package io.github.laurarebecasantos.lesson0089;
+
+class Lesson89 {
+	
+	
+/* 
+JAVA 8 - Lambdas Expressions
+
+Feature - Compact profiles (modularization)
+Feature - New JavaScript engine (Nashhorn)
+Feature - Permanet Generation Removal
+Feature - Parallel Matrix Classification
+Feature - Encoding / Decoding (Base64)
+Feature - New API Date / Time
+Feature - Repeated actions
+Feature - Massive collections operations
+Feature - Security fixes
+Feature - Enhanced in JDBC (database access)
+*/
+	
+	
+}
