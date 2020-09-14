a3f1a9cf558b74db76f0211ccf8c58c3b08850de
laurarebecasantos <laurarebecasantos@gmail.com>
2020-09-14 14:54:15 -0300

    Lamda Syntax

---
+++ src/io/github/laurarebecasantos/lesson0092/Lesson0092.java
@@ -0,0 +1,23 @@
+package io.github.laurarebecasantos.lesson0092;
+
+class Lesson0092 {
+	
+	public static void main(String[] args) {
+	
+		//Parameters of list:
+	
+	// Runnable r1;
+	// r1 = (int x, int y) -> x + y;
+	// r1 = (int x, int y) -> {x + y};
+	
+	
+	/* Lesson0092
+	 * Lamda Syntax
+	 * 
+	 * () -- Arguments of list
+	 * -> -- Arrow - indicates that it is lambda expression
+	 * x+y; -- Body
+	 * 
+	 */
+	}
+}
