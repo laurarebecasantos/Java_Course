2e71002a8695bc3390ee93da321766bce80fbe63
laurarebecasantos <laurarebecasantos@gmail.com>
2020-09-14 14:52:32 -0300

    Lambdas Expressions

---
+++ src/io/github/laurarebecasantos/lesson0090/Lesson0090.java
@@ -0,0 +1,19 @@
+package io.github.laurarebecasantos.lesson0090;
+
+class Lesson0090 {
+
+	
+	/**
+	 * Lambda expressions: is any object that implements one of these interfaces.
+	 *
+	 * Function: Function that takes a single argument.
+	 * Predicate: Function that receives a single parameter and returns a 
+     * 	boolean.
+	 * Consumer: function that receives a single parameter and does not return anything.
+	 */
+	
+	/* Lesson0090
+	 * Lambdas Expressions
+	 * 
+	 */
+}
