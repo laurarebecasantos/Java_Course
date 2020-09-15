12ee6c9916339548ca161c0af04697451080c204
laurarebecasantos <laurarebecasantos@gmail.com>
2020-09-14 22:55:52 -0300

    Introduction to the stream

---
+++ src/io/github/laurarebecasantos/lesson0096/Lesson0096.java
@@ -0,0 +1,21 @@
+package io.github.laurarebecasantos.lesson0096;
+
+class Lesson0096 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Lesson0096
+		 * Introduction to the stream
+		 * 
+		 * It is a sequence of elements. In this, there are functions for mass
+		 * operations, thus promoting the easy manipulation of the elements.
+		 * 
+		 * Stream, another feature implemented in version 8 of Java that provides the
+		 * ability to implement code in a much simpler way and that will run in a much
+		 * more performance way
+		 * 
+		 */
+	}
+
+}
